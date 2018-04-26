a1214ff568ed41b798a156f8b9a04d046e42d9bf
Unknown <maximilien.legras@viacesi.fr>
2018-04-26 15:51:30 +0200

    Ajout de l'id du film dans le lien

--- js/index.js
+++ js/index.js
@@ -4,13 +4,8 @@ $(document).ready(function() {
 
     $.ajax(api_settings).done(function(response){
         $.each(response.results, function(key, movie) {
-            // arrLatestMovies.push({
-            //     id: movie.id,
-            //     title: movie.title,
-            //     poster: url_image_small + movie.poster_path,
-            //     backdrop: url_image_original + movie.backdrop_path
-            // });
-        $('section').append('<div id="'+movie.id+'" class="poster"><a href="#"><img src="'+url_image_small + movie.poster_path+'" alt="'+movie.title+'" title="'+movie.title+'"></a></div>')
+
+            $('section').append('<div id="'+movie.id+'" class="poster"><a href="/detail.html?id='+movie.id+'"><img src="'+url_image_small + movie.poster_path+'" alt="'+movie.title+'" title="'+movie.title+'"></a></div>')
 
         });
     });
