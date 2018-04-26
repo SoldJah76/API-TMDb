b490b942fa2a1fa2eaec3ae774a3bbd8955b4ba4
Unknown <maximilien.legras@viacesi.fr>
2018-04-26 14:06:38 +0200

    Jquery pour la page index

---
+++ js/index.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+    api_settings.url = r_last_movie + "&page=1";
+
+    $.ajax(api_settings).done(function(response){
+        $.each(response.results, function(key, movie) {
+            // arrLatestMovies.push({
+            //     id: movie.id,
+            //     title: movie.title,
+            //     poster: url_image_small + movie.poster_path,
+            //     backdrop: url_image_original + movie.backdrop_path
+            // });
+        $('section').append('<div id="'+movie.id+'" class="poster"><img src="'+url_image_small + movie.poster_path+'" alt="'+movie.title+'" title="'+movie.title+'"></div>')
+
+        });
+    });
+
+});
